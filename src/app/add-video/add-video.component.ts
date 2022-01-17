b5bea2735d162512c4459cf6dd009ebca9e7cb2a
Ajay Wagh <54326598+Ajaywagh2298@users.noreply.github.com>
2022-01-17 15:05:31 +0530

    commit

--- src/app/add-video/add-video.component.ts
+++ src/app/add-video/add-video.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-video',
-  templateUrl: './add-video.component.html',
+  templateUrl:'add-video.component.html',
   styleUrls: ['./add-video.component.scss']
 })
 export class AddVideoComponent implements OnInit {
