bfa4169972b435a8fcbb7b442ba8b132b79ee709
Ajay Wagh <54326598+Ajaywagh2298@users.noreply.github.com>
2022-01-17 16:42:48 +0530

    commit

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,61 +1,21 @@
-let express = require('express'),
-  path = require('path'),
-  mongoose = require('mongoose'),
-  cors = require('cors'),
-  bodyParser = require('body-parser'),
-  dbConfig = require('./database/db');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017').then(r => {
+  console.log('database not found')
+})
+const cors = require('cors')
+const app = express()
 
-// Connecting with mongo db
-mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.db, {
-  useNewUrlParser: true
-}).then(() => {
-    console.log('Database sucessfully connected')
-  },
-  error => {
-    console.log('Database could not connected: ' + error)
-  }
-)
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+app.use(`/`,require('./routes/index.route'));
 
-// Setting up port with express js
-const lectureRoute = require('../Back-End/routes/lecture.route')
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(cors());
-//app.use(express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
-//app.use('/', express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
-app.use('/api', lectureRoute)
 
-// Create port
-const port = process.env.PORT || 4000;
-const server = app.listen(port, () => {
+const port = 8022 || process.env.PORT
+
+
+app.listen(port, () => {
   console.log('Connected to port ' + port)
 })
-
-// Find 404 and hand over to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-
-// Connecting with mongo db
-mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.db, {
-  useNewUrlParser: true
-}).then(() => {
-    console.log('Database sucessfully connected')
-  },
-  error => {
-    console.log('Database could not connected: ' + error)
-  }
-)
-
-// error handler
-app.use(function (err, req, res, next) {
-  console.error(err.message); // Log error message in our server's console
-  if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
-  res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
-});
